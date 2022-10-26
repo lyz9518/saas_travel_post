86a0c9ec0322b7a4a4ec3061a51105363067d42e
Arthur Liu <lyz9518@163.com>
2022-10-26 16:30:50 -0400

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -33,3 +33,17 @@ ActiveRecord::Schema.define(version: 20111119180638) do
   end
 
 end
+
+
+ActiveRecord::Schema.define(version: 20111119180638) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.text     "description"
+    t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,34 @@ posts = [
   { post_id: '3', zipcode: '10463', creator_id: '2', description: 'hello USA' }
 ]
 
+users = [
+  { user_id: "1", user_name: "lion", first_name: "John", last_name: "Smith", password: "123"},
+  { user_id: "2", user_name: "king", first_name: "Adam", last_name: "Mctigue", password: "123"}
+]
+
 posts.each do |post|
   Post.create!(post)
+end
+
+users.each do |user|
+  User.create!(user)
+end
+
+
+
+movies = [
+  { title: 'Aladdin', rating: 'G', release_date: '25-Nov-1992' },
+  { title: 'The Terminator', rating: 'R', release_date: '26-Oct-1984' },
+  { title: 'When Harry Met Sally', rating: 'R', release_date: '21-Jul-1989' },
+  { title: 'The Help', rating: 'PG-13', release_date: '10-Aug-2011' },
+  { title: 'Chocolat', rating: 'PG-13', release_date: '5-Jan-2001' },
+  { title: 'Amelie', rating: 'R', release_date: '25-Apr-2001' },
+  { title: '2001: A Space Odyssey', rating: 'G', release_date: '6-Apr-1968' },
+  { title: 'The Incredibles', rating: 'PG', release_date: '5-Nov-2004' },
+  { title: 'Raiders of the Lost Ark', rating: 'PG', release_date: '12-Jun-1981' },
+  { title: 'Chicken Run', rating: 'G', release_date: '21-Jun-2000' },
+]
+
+movies.each do |movie|
+  Movie.create!(movie)
 end
