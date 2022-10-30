2632cbd48448863a403c7eebba0835e118c90526
Arthur Liu <lyz9518@163.com>
2022-10-29 23:23:01 -0400

    add back potaton app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Goodpoints::Application.configure do
+Rottenpotatoes::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Goodpoints::Application.configure do
+Rottenpotatoes::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
