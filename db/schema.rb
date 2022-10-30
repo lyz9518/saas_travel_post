d710f143fd053d049f87112cc04d3f24f7eab656
Arthur Liu <lyz9518@163.com>
2022-10-30 00:16:06 -0400

    test

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20111119180638) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "zipcode"
     t.string "title"
