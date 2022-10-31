7679380e8304929e63c85415d2317b5372260a1d
klauscli <klauscli@tencent.com>
2022-10-31 15:45:30 -0400

    Finish Rspec for controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -40,49 +40,53 @@ describe PostsController do
     it 'creates a new post and redirects to the index page' do
       orig_posts_count = Post.all.count
       request.session[:user_id] = 1
-      post :create, :post => {:zipcode=>"10463", :title=>"AAA", :username=>"XXX"}
+      post :create, :post => {:zipcode=>"10463", :title=>"AAA", :description=>"XXX"}
       expect(Post.all.count).to eq(orig_posts_count + 1)
 
       expect(response).to redirect_to(posts_url)
     end
   end
-  #
-  # describe 'DELETE' do
-  #   let!(:movie) { Movie.create!(title: 'Transformer', director: 'Zijian Zhang') }
-  #
-  #   it 'deletes a movie and redirects to the home page and alert' do
-  #     orig_movies_count = Movie.all.count
-  #     delete :destroy, id: movie.id
-  #     expect(Movie.all.count).to eq(orig_movies_count - 1)
-  #
-  #     expect(response).to redirect_to(movies_path)
-  #     expect(flash[:notice]).to eq("Movie 'Transformer' deleted.")
-  #   end
-  # end
-  #
-  # describe 'Edit' do
-  #   let!(:movie) { Movie.create!(title: 'Transformer', director: 'Zijian Zhang') }
-  #
-  #   before do
-  #     get :edit, id: movie.id
-  #   end
-  #
-  #   it 'should edit a movie and redirect' do
-  #     expect(assigns(:movie)).to eql(movie)
-  #     expect(response).to render_template('edit')
-  #   end
-  # end
-  #
-  # describe 'Update' do
-  #   let!(:movie) { Movie.create!(title: 'Transformer', director: 'Zijian Zhang') }
-  #
-  #   it 'updates a movie and redirects to the movie page' do
-  #     put :update, id: movie.id, movie: {:title=>"TransformerII"}
-  #     movie.reload
-  #     expect(movie.title).to eql('TransformerII')
-  #
-  #     expect(response).to redirect_to(movie_path(movie))
-  #     expect(flash[:notice]).to eq("TransformerII was successfully updated.")
-  #   end
-  # end
+
+  describe 'DELETE' do
+    let!(:user) { User.create!(user_name: 'abc', first_name: 'Columbia', last_name: "Lion", password: "123")}
+    let!(:post) { Post.create!(title: 'Hi', zipcode: '10463', creator_id: "")}
+  
+    it 'deletes a post and redirects to the home page and alert' do
+      orig_post_count = Post.all.count
+      delete :destroy, id: post.id
+      expect(Post.all.count).to eq(orig_post_count - 1)
+  
+      expect(response).to redirect_to(posts_path)
+      expect(flash[:notice]).to eq("post 'Hi' deleted.")
+    end
+  end
+  
+  describe 'Edit' do
+
+    let!(:user) { User.create!(user_name: 'abc', first_name: 'Columbia', last_name: "Lion", password: "123")}
+    let!(:new_post) { Post.create!(title: 'Hi', zipcode: '10463', creator_id: "")}
+
+    before do
+      get :edit, id: new_post.id
+    end
+  
+    it 'should edit a post and redirect' do
+      expect(assigns(:post)).to eql(new_post)
+      expect(response).to render_template('edit')
+    end
+  end
+  
+  describe 'Update' do
+    let!(:user) { User.create!(user_name: 'abc', first_name: 'Columbia', last_name: "Lion", password: "123")}
+    let!(:post) { Post.create!(title: 'Hi', zipcode: '10463', creator_id: user.id)}
+  
+    it 'updates a post and redirects to the post page' do
+      put :update, id: post.id, post: {:title=>"HiII"}
+      post.reload
+      expect(post.title).to eql('HiII')
+  
+      expect(response).to redirect_to(post_path(post))
+      expect(flash[:notice]).to eq("HiII was successfully updated.")
+    end
+  end
 end
