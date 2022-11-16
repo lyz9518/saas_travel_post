364b9dd206fa5b54acec6b1c56c77461812e25c4
klauscli <klauscli@tencent.com>
2022-11-16 14:21:47 -0500

    Add Css style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,17 @@ class UsersController < ApplicationController
     end
   
     def create
-      @user = User.create(params.require(:user).permit(:user_name, :first_name, :last_name, :password))
-      session[:user_id] = @user.id
-      flash["notice"] = "New User Created"
-      redirect_to '/posts'
+      if params["user"]["user_name"].empty?
+        flash["notice"] = "User Name can't be empty"
+        redirect_to(:back)
+      elsif params["user"]["password"].empty?
+        flash["notice"] = "Password can't be empty"
+        redirect_to(:back)
+      else
+        @user = User.create(params.require(:user).permit(:user_name, :first_name, :last_name, :password))
+        session[:user_id] = @user.id
+        flash["notice"] = "New User Created"
+        redirect_to '/posts'
+      end
     end
   end
