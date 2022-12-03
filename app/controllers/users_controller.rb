103de9e5dd9b85779e40cc887abc799f5b9a6d2f
klauscli <klauscli@tencent.com>
2022-12-03 17:31:00 -0500

    Debug::duplicated username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,9 @@ class UsersController < ApplicationController
       elsif params["user"]["password"].empty?
         flash["notice"] = "Password can't be empty"
         redirect_to '/users/new'
+      elsif User.exists?(user_name: params["user"]["user_name"])
+        flash["notice"] = "Username already exist"
+        redirect_to '/users/new'
       else
         @user = User.create(params.require(:user).permit(:user_name, :first_name, :last_name, :password))
         session[:user_id] = @user.id
